3be12dc9b175e2d8720fe524f827a022cc26a8d8
moydulhaque <95543844+moydulhaque@users.noreply.github.com>
2022-12-28 00:34:04 +0600

    update

---
+++ dist/assets/About-9cb093b1.js
@@ -0,0 +1,1 @@
+import{_ as t}from"./index-e7c130ba.js";const o={name:"About"};function e(r,n,s,a,c,_){return" About Page "}const u=t(o,[["render",e]]);export{u as default};
---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import {RouterLink, RouterView} from 'vue-router'
+</script>
+
+<template>
+
+  <nav class="p-4 mb-4">
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/about">About</RouterLink>
+  </nav>
+  <hr>
+
+  <RouterView/>
+</template>
+
+<style scoped>
+
+</style>
